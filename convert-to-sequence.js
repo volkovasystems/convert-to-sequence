526f4e50989e1ffa18b07b1dd01adf9481db09f1
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-06-26 21:58:29 +0800

    Updated convertToSequence procedure.

--- convert-to-sequence.js
+++ convert-to-sequence.js
@@ -2,13 +2,12 @@
 	var window = window || null;
 	var exports = exports || { };
 	var base = window || exports;
-	//try{ var base = window; }catch( error ){ var base = exports; }
 	if( typeof define != "function" ){ var define = require( "amdefine" )( module ); }
 	var include = require( "requirejs" );
 	include.config( {
 		"paths": {
-			"convertToSequence": "/convert-to-sequence",
-			"work": "/work/work"
+			"convertToSequence": "./convert-to-sequence",
+			"work": "./work/work"
 		},
 		"nodeRequire": require
 	} );
@@ -18,11 +17,10 @@
 			"work"
 		],
 		function construct( fs ){
-			console.log( arguments.length );
 			var convertToSequence = function convertToSequence( sequence, dictionary, separator, callback ){
 			};
 
 			return convertToSequence;
 		} );
-	include( [ "convertToSequence" ] );
+	include( "convertToSequence" );
 } )( );
--- test.js
+++ test.js
@@ -1,7 +1,5 @@
 var include = require( "requirejs" );
 include( [ "./convert-to-sequence.js" ],
 	function(  ){
-		var util = require( "util" );
-		var convertToSequence = include( "convertToSequence" );
-		console.log( util.inspect( convertToSequence ) );
+		
 	} );
