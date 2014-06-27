07607727d54a44a8dab4d0aede06e39d8cd234f0
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-06-27 10:17:18 +0800

    Modified the structure.

--- convert-to-sequence.js
+++ convert-to-sequence.js
@@ -1,26 +1,5 @@
-( function module( ){
-	var window = window || null;
-	var exports = exports || { };
-	var base = window || exports;
-	if( typeof define != "function" ){ var define = require( "amdefine" )( module ); }
-	var include = require( "requirejs" );
-	include.config( {
-		"paths": {
-			"convertToSequence": "./convert-to-sequence",
-			"work": "./work/work"
-		},
-		"nodeRequire": require
-	} );
-	define( "convertToSequence",
-		[
-			"fs",
-			"work"
-		],
-		function construct( fs ){
-			var convertToSequence = function convertToSequence( sequence, dictionary, separator, callback ){
-			};
+/*:
+*/
+var convertToSequence = function convertToSequence( sequenceIndex, dictionary, callback, separator ){
 
-			return convertToSequence;
-		} );
-	include( "convertToSequence" );
-} )( );
+};
