04080c7b5c8622c1e3452bc69acfa20653fbebc6
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-06-29 07:10:48 +0800

    Added temporary important codes.

--- convert-to-sequence.js
+++ convert-to-sequence.js
@@ -3,3 +3,6 @@
 var convertToSequence = function convertToSequence( sequenceIndex, dictionary, callback, separator ){
 
 };
+
+var work = require( "./work/work.js" );
+module.exports = convertToSequence;
